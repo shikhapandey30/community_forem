ab807646368cbe69e42aa15a1eb397ea1785d4b4
kajal <kajal_priyadarshani@seologistics.com>
2015-10-12 18:37:01 +0530

    changes

--- app/admin/category.rb
+++ app/admin/category.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register Category do
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
 # permit_params :list, :of, :attributes, :on, :model
- permit_params :name
+# permit_params :name
 # or
 #
 # permit_params do
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ActiveRecord::Base
-	has_many :categorable_categories,:dependent => :destroy, :as => :categorable
+	# has_many :categorable_categories,:dependent => :destroy, :as => :categorable
 
 end
