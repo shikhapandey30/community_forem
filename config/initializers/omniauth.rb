fad5c7c5d25b5bd45fb1bb383f032243abe8c3b6
kajal <kajal_priyadarshani@seologistics.com>
2015-10-13 09:15:19 +0530

    changes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -6,7 +6,7 @@ if Rails.env=='development'
    provider :google_oauth2, '916993778195-g8us044cdqf041giiu4nj3jnmthcd2is.apps.googleusercontent.com', 'fZtHAyE-zj51Qg5SE6Y-RVIW' 
 else
 	provider :facebook, '905207316223625', 'f61eda3cc0a382d542bfc0bc3ce9eeef'
-    provider :google_oauth2,'1073020948172-36om8fdogfq73knlp9a7lqb0vsg304q1.apps.googleusercontent.com', 'TIjBo9zIbqa1vVvNo3ZpTRgS' 
+    provider :google_oauth2,' 1073020948172-36om8fdogfq73knlp9a7lqb0vsg304q1.apps.googleusercontent.com','TIjBo9zIbqa1vVvNo3ZpTRgS' 
     provider :twitter, 'sCximTbSsmWPEZtl2qhlGaLZN', 'p4pgwnn5H5tlPJBGFjmFRZRY6b0IewOdNGutJBYYKGRoa7clFi'
 end
 end
