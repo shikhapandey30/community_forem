7ee1a093b6ad553651d37f36fca8d17a22fd97d3
kajal <kajal_priyadarshani@seologistics.com>
2015-10-15 18:36:13 +0530

    changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,7 @@ class UsersController < ApplicationController
     @posts=pub_post+self_post
 	end
   def user_category
+    @category=Category.all
     if current_user.categorables_categories.present?
       cat=current_user.categorables_categories.uniq
       @all_cat=cat.collect{|f| f.name}
@@ -27,11 +28,13 @@ class UsersController < ApplicationController
     
     @category_ids=params[:category_ids]
     if @category_ids.present?
+      current_user.categorables.delete_all
      @category_ids.each do |f|
       category=Categorable.create(:categorable_type=>'Category',:categorable_id=>f,:user_id=>current_user.id,:category_id=>f)
     end
+     redirect_to dashboard_path, notice: 'Categories Added successfully'
   end
-    @category=Category.all
+ 
   end
 
 	def social_login
