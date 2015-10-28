6f2a15770ce1e11cfc6e34889b64c506e13dd242
kandarp_sharma <kandarp_sharma@seologistics.com>
2015-10-28 10:53:00 +0530

    fixed issue with categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ActiveRecord::Base
 	# has_many :categorable_categories,:dependent => :destroy, :as => :categorable
-     has_many :categorable
+     has_many :categorables
      has_many :users, through: :categorables
 end
