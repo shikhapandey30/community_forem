1882149c6ba6672bfcc176f3bf22d65401b0d6b0
Vishal Tyagi <vishal_tyagi@seologistics.com>
2015-10-29 13:48:26 +0530

    remove column to user

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -18,7 +18,15 @@ ActiveAdmin.register User do
      end
        f.actions
  end
-
+index do
+    selectable_column
+    id_column
+    column :email
+    # column :current_sign_in_at
+    # column :sign_in_count
+    # column :created_at
+    actions
+  end
 
 
 
