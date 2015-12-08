b3b18799b9bd8594025760b379df9e17f494b181
hemant <hemant_gupta@seologistics.com>
2015-12-08 11:51:13 +0530

    pull and solve conflict

--- app/uploaders/profile_uploader.rb
+++ app/uploaders/profile_uploader.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-class PostUploader < CarrierWave::Uploader::Base
+class ProfileUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
@@ -15,10 +15,10 @@ class PostUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
-    def default_url
-    # '../images/default_post.jpeg'
-   end
 
+  def default_url
+    'images/default_logo.png'
+   end
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
