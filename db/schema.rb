f3db2c2bcb29e29b9790962a4cc97596af6a452f
hemant <hemant_gupta@seologistics.com>
2015-12-10 19:43:48 +0530

    add popup msg

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151210070553) do
+ActiveRecord::Schema.define(version: 20151210101505) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "trackable_id",   limit: 4
@@ -135,6 +135,7 @@ ActiveRecord::Schema.define(version: 20151210070553) do
     t.string   "start_work_date",  limit: 255
     t.string   "worked_till",      limit: 255
     t.text     "description",      limit: 65535
+    t.boolean  "current_company"
   end
 
   create_table "followings", force: :cascade do |t|
