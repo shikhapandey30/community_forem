9aaacab905a3e6964f21aa271338b457c8bae4e3
Arvind Kushwah <arvind_kushwah@seologistics.com>
2015-12-11 18:08:23 +0530

    ad migration

---
+++ db/migrate/20151211123759_addtopicinpost.rb
@@ -0,0 +1,11 @@
+class Addtopicinpost < ActiveRecord::Migration
+  def change
+  	 add_column 		:posts, :image, :string
+  	 add_column 		:posts, :video, :string
+  	 add_column 		:posts, :site_link, :string
+  	 add_column 		:posts, :topic, :string
+  	 add_column 		:posts, :start_date, :date
+  	 remove_column   :posts, :slug
+  	 rename_column 	:posts, :post_text, :description
+  end
+end
