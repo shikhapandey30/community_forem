7d099387c11b1a8aa8fb89a0a5ae53fb2077ffd2
hemant <hemant_gupta@seologistics.com>
2015-12-14 19:25:17 +0530

    change for category

--- app/assets/javascripts/js/validate.js
+++ app/assets/javascripts/js/validate.js
@@ -333,7 +333,7 @@ $.extend( $.validator, {
         number: "Please enter a valid number.",
         digits: "Please enter only digits.",
         creditcard: "Please enter a valid credit card number.",
-        equalTo: "Please enter the same value again.",
+        equalTo: "Your password didn't match. Please enter again.",
         maxlength: $.validator.format( "Please enter no more than {0} characters." ),
         minlength: $.validator.format( "Please enter at least {0} characters." ),
         rangelength: $.validator.format( "Please enter a value between {0} and {1} characters long." ),
