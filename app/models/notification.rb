5eef8291b11bb5de6b6fb70a8e39ec6366ffbc89
Arvind Kushwah <arvind_kushwah@seologistics.com>
2015-12-15 15:29:24 +0530

    add mnotification functionality

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,4 +1,9 @@
 class Notification < ActiveRecord::Base
-	belongs_to :user
+	 belongs_to :recepient, class_name: "User"
+     belongs_to :user, class_name: "User"
+	 belongs_to :notificable, :polymorphic => true
+	 scope :unread, ->{ where(accept: false) }
+	 scope :read, ->{ where(accept: true) }
+	 scope :messages, ->{ where(notificable_type: 'Message') }
 	attr_accessor :sender_id
 end
