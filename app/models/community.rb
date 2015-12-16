f732dd55b07957dfeafa6097f92753c5e12f4ec7
hemant <hemant_gupta@seologistics.com>
2015-12-16 16:37:39 +0530

    add user filter

--- app/models/community.rb
+++ app/models/community.rb
@@ -1,6 +1,13 @@
 class Community < ActiveRecord::Base
 	belongs_to :user
-    has_one :upload, as: :uploadable, dependent: :destroy
-    accepts_nested_attributes_for :upload, :allow_destroy => true  
-    has_many :members,:dependent => :destroy, :as => :invitable
+  has_one :upload, as: :uploadable, dependent: :destroy
+  accepts_nested_attributes_for :upload, :allow_destroy => true  
+  has_many :members,:dependent => :destroy, :as => :invitable
+  def self.search(search)
+    if search
+      where('topic LIKE ?', "%#{search}%")
+    else
+      all
+    end
+  end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,12 @@ class Group < ActiveRecord::Base
 	has_one :upload
 	accepts_nested_attributes_for :upload, :allow_destroy => true  
     has_many :members,:dependent => :destroy, :as => :invitable
+
+  def self.search(search)
+    if search
+      where('topic LIKE ?', "%#{search}%")
+    else
+      all
+    end
+  end
 end
