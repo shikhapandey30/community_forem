20b1c61ad4cbefa28aa9e6e719de52a345900998
Pavitra Singh <pavitra_singh@seologistics.com>
2015-12-16 14:51:00 +0530

    group new page

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,5 +1,7 @@
 class Upload < ActiveRecord::Base
 	belongs_to :uploadable, polymorphic: true
 	mount_uploader :file, PostUploader
-    mount_uploader :image, PostUploader    
+    mount_uploader :image, PostUploader
+    mount_uploader :file, GroupUploader
+    mount_uploader :image, GroupUploader    
 end
