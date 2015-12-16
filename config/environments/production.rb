8bb0f37e6334fdd4e3934e5c30f39865426c447a
Arvind Kushwah <arvind_kushwah@seologistics.com>
2015-12-16 15:45:09 +0530

    chnage

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,7 @@ Rails.application.configure do
   config.log_level = :debug
    config.active_record.dump_schema_after_migration = false
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host:'108.174.155.117:3004'}
+  config.action_mailer.default_url_options = { host:'http://tgs-network.herokuapp.com/'}
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
