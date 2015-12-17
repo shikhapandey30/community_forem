54d451296c537633b27a1b4d8fc93a38977b6912
Arvind Kushwah <arvind_kushwah@seologistics.com>
2015-12-17 18:52:25 +0530

    chnage

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -4,12 +4,14 @@ class FriendshipsController < ApplicationController
   # GET /friendships
   # GET /friendships.json
   def index
-    @friendships = current_user.friendships +  current_user.inverse_friendships
-    # @friends =  current_user.friends +  current_user.inverse_friends
-    @friends = User.where(:id => current_user.friendships.collect(&:friend_id))
-    @inverse_friends = User.where(:id => current_user.inverse_friendships.collect(&:user_id))
-    @friends = (@friends + @inverse_friends)
-    @friends =@friends.paginate(:page => params[:page], :per_page => 10)
+    # @friendships = current_user.friendships +  current_user.inverse_friendships
+    # # @friends =  current_user.friends +  current_user.inverse_friends
+    # @friends = User.where(:id => current_user.friendships.collect(&:friend_id))
+    # @inverse_friends = User.where(:id => current_user.inverse_friendships.collect(&:user_id))
+    # @friends = (@friends + @inverse_friends)
+    # @friends =@friends.paginate(:page => params[:page], :per_page => 10)
+     @friends = current_user.friends + current_user.inverse_friends
+     @friends.delete(current_user)
   end
   # GET /friendships/1
   # GET /friendships/1.json
