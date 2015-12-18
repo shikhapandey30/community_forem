799fbb83c130194396c60d37bda42433ba6cdcdb
hemant <hemant_gupta@seologistics.com>
2015-12-18 15:23:23 +0530

    fix edit/delete post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -67,7 +67,7 @@ class PostsController < ApplicationController
   def destroy
     @post.destroy
     respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Post is successfully destroyed.' }
+      format.html { redirect_to dashboard_path, notice: 'Post is successfully destroyed.' }
       format.json { head :no_content }
     end
   end
