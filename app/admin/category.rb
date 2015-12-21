5e990c73947bc6cff6d908012a8bacbda6165c9e
Vishal Tyagi <vishal_tyagi@seologistics.com>
2015-12-21 18:53:20 +0530

    add resourse in active admin

---
+++ app/admin/category.rb
@@ -0,0 +1,47 @@
+ActiveAdmin.register Category do
+  preserve_default_filters!
+  # menu label: 'Caregory'
+  permit_params :name, :image, :icon
+
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if resource.something?
+#   permitted
+# end
+index do
+    selectable_column
+    id_column
+    column :name
+    column :image
+    column :icon
+    column :current_sign_in_at
+    column :sign_in_count
+    column :created_at
+    actions
+  end
+
+  filter :name
+  filter :current_sign_in_at
+  filter :sign_in_count
+  filter :created_at
+
+  form do |f|
+    f.inputs "Admin Details" do
+      f.input :user
+      f.input :image
+      f.input :icon
+      # f.input :password
+      # f.input :password_confirmation
+    end
+    f.actions
+  end
+
+
+end
