28a40e6d27e224fff0470fb941904a8cb1c2098b
Arvind Kushwah <arvind_kushwah@seologistics.com>
2015-12-21 14:10:11 +0530

    update rply

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,4 +1,6 @@
 class RepliesController < ApplicationController
+    before_action :set_reply, only: [:update, :destroy]
+
 	def create
 		@comment = Comment.find(params[:comment_id])
 		params[:reply][:user_id] = current_user.id
@@ -6,8 +8,24 @@ class RepliesController < ApplicationController
 		@replies = @comment.replies
 	end
 
+	def update
+	  respond_to do |format|
+	    if @reply.update(reply_params)
+	      format.html { redirect_to @reply, notice: 'reply is successfully updated.' }
+	      format.js { render :update, status: :ok, location: @reply}
+	     # format.json { render :show, status: :ok, location: @comment }
+	    else
+	      format.html { render :edit }
+	     format.json { render json: @comment.errors, status: :unprocessable_entity }
+	    end
+	  end
+  end
+
+
 	private
-		
+	def set_reply
+      @reply = Reply.find(params[:id])
+    end
 	def reply_params
 		params.require(:reply).permit(:comment_id, :user_id, :body)
 	end
