26e9d544f5381dc32f74c190898549f9dd1d06e5
Arvind Kushwah <arvind_kushwah@seologistics.com>
2015-12-21 19:01:59 +0530

    chnage

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,7 @@
 class Profile < ActiveRecord::Base
 	belongs_to :user
 	mount_uploader :image, UserUploader 
+	belongs_to :country, :foreign_key => "country"
 	# validates_presence_of :first_name,:gender,:dob,:screen_name,:profile_type
     # validates :screen_name, uniqueness: true, :allow_blank => true
 end
