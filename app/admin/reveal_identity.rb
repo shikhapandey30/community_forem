0c22a57318e3188d8de24cac572cefcf6e142520
kandarp_sharma <kandarp_sharma@seologistics.com>
2015-12-22 15:11:07 +0530

    Change Admin Skin

--- app/admin/reveal_identity.rb
+++ app/admin/reveal_identity.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register RevealIdentity do
+	menu false
 permit_params :sender_id, :user_id, :body, :accept
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
