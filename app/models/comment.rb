11b0059b609f0bea059dee20b539490cf5211f9c
Arvind Kushwah <arvind_kushwah@seologistics.com>
2015-12-22 15:16:08 +0530

    chnage

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,9 +2,10 @@ class Comment < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :post
 	belongs_to :meeting_room
-	 has_many :likes, :as => :likable
-  belongs_to :commentable, :polymorphic => true
-  belongs_to :parent, :class_name => 'Comment', :foreign_key => :parent_id
+	has_many :likes, :as => :likable, dependent: :destroy
+	has_many :dislikes, :as => :dislikable, dependent: :destroy
+    belongs_to :commentable, :polymorphic => true
+    belongs_to :parent, :class_name => 'Comment', :foreign_key => :parent_id
 	has_many :children, :class_name => 'Comment'
 	has_many :replies, dependent: :destroy
 end
