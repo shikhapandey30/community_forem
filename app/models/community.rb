f996c23b63d06e4752b458406fc84008dc4b1ede
kandarp_sharma <kandarp_sharma@seologistics.com>
2015-12-23 17:52:30 +0530

    Admin changes

--- app/models/community.rb
+++ app/models/community.rb
@@ -1,7 +1,7 @@
 class Community < ActiveRecord::Base
 	belongs_to :user
   has_one :upload, as: :uploadable, dependent: :destroy
-  accepts_nested_attributes_for :upload, :allow_destroy => true  
+  accepts_nested_attributes_for :upload, :allow_destroy => true,  :reject_if => lambda { |c| c[:image].blank? }
   has_many :members,:dependent => :destroy, :as => :invitable
   def self.search(search)
     if search
