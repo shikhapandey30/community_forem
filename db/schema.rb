26245d8891f6da6a321b125348a3de53fe2191ab
Arvind Kushwah <arvind_kushwah@seologistics.com>
2015-12-23 13:33:43 +0530

    chnage

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151222074926) do
+ActiveRecord::Schema.define(version: 20151223043437) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace",     limit: 255
@@ -137,6 +137,15 @@ ActiveRecord::Schema.define(version: 20151222074926) do
     t.integer  "user_id",     limit: 4
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.string   "phone_no",   limit: 255
+    t.string   "email",      limit: 255
+    t.text     "body",       limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
   create_table "contests", force: :cascade do |t|
