c3bc9062e15633eb96939efca457aca0645b0cef
Arvind Kushwah <arvind_kushwah@seologistics.com>
2015-12-24 20:00:08 +0530

    chnage

--- app/models/contest.rb
+++ app/models/contest.rb
@@ -8,7 +8,7 @@ class Contest < ActiveRecord::Base
 	has_many :comments,:dependent => :destroy, :as => :commentable
     has_many :followings, as: :followable, :dependent => :destroy
 	# validates_presence_of :topic_id
-	validates_presence_of :category_id
+	validates_presence_of :category_id, :start_date, :end_date
 	has_one :upload, as: :uploadable, dependent: :destroy
 	accepts_nested_attributes_for :upload, :allow_destroy => true
 	def img
