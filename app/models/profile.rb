57cc7b8fa499167c2e2e8139676ad73e47c3dba1
Pavitra Singh <pavitra_singh@seologistics.com>
2015-12-31 11:58:43 +0530

    small change

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,11 +1,16 @@
 class Profile < ActiveRecord::Base
-	belongs_to :user
-	mount_uploader :image, UserUploader 
-	belongs_to :contry, class_name: "Country", :foreign_key => "country"
-	# validates_presence_of :first_name,:gender,:dob,:screen_name,:profile_type
-    # validates :screen_name, uniqueness: true, :allow_blank => true
 
-    def country
-    	self.contry
-    end
+	## Associations
+	belongs_to :user
+	belongs_to :contry, class_name: "Country", :foreign_key => "country"
+
+	## Image uploading
+	mount_uploader :image, UserUploader
+	
+	# validates_presence_of :first_name,:gender,:dob,:screen_name,:profile_type
+  # validates :screen_name, uniqueness: true, :allow_blank => true
+
+  def country
+  	self.contry
+  end
 end
