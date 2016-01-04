ea0b6ba43512eb2be24a35b45309361df228ebbd
Pavitra Singh <pavitra_singh@seologistics.com>
2016-01-04 10:25:37 +0530

    more changes

--- app/controllers/reveal_identities_controller.rb
+++ app/controllers/reveal_identities_controller.rb
@@ -1,6 +1,9 @@
 class RevealIdentitiesController < ApplicationController
-   before_filter :authenticate_user!
 
+	# filters
+  before_filter :authenticate_user!
+
+  # user reveal identity
   def index
   	@reveal_identities = current_user.reveal_identities.order("created_at desc") rescue []
   end
