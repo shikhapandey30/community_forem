f9bf3ea8380b0874e51972877ef6e2b07a3f70f9
Arvind Kushwah <arvind_kushwah@seologistics.com>
2016-01-04 09:38:47 +0530

    add category slug

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,7 +7,7 @@ class CategoriesController < ApplicationController
 
 	# fetching all posts for particular category
 	def show
-		@category = Category.find(params[:id])
+		@category = Category.friendly.find(params[:id])
 		@posts = @category.posts.order("updated_at desc")
 		@comment = Comment.new
 	end
