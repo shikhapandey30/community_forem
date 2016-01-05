4dee7d7b1fe8ff8ea5bcae74429ba03da8469763
Arvind Kushwah <arvind_kushwah@seologistics.com>
2016-01-05 15:15:53 +0530

    change

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -1,2 +1,14 @@
 module NotificationsHelper
+
+	def notification_html(reciver_id)
+		@current_user = User.find(reciver_id)
+		notifications = @current_user.notifications.unread 
+        messages = @current_user.notifications.messages.unread 
+        friend_requests = @current_user.notifications.friend_requests.unread 
+        notifications = notifications - messages - friend_requests
+
+		"<span> #{@current_user.screen_name} dsdsdsdd </span>		
+		
+		"
+	end
 end
