e76f80068dfcdad635c0fb413a293960e2a6537d
hemant <hemant_gupta@seologistics.com>
2016-01-07 18:27:49 +0530

    add img and doc for chat

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160107090505) do
+ActiveRecord::Schema.define(version: 20160107102616) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace",     limit: 255
@@ -370,6 +370,7 @@ ActiveRecord::Schema.define(version: 20160107090505) do
     t.boolean  "archive",                       default: false
     t.boolean  "is_read",                       default: false
     t.datetime "read_at"
+    t.integer  "by_user",         limit: 4
   end
 
   add_index "messages", ["conversation_id"], name: "index_messages_on_conversation_id", using: :btree
