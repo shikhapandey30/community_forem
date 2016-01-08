4c9bafdff23e2a9e693dee6f44fc3ad9f080d7e0
Pavitra Singh <pavitra_singh@seologistics.com>
2016-01-08 13:06:11 +0530

    change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,15 @@
 class ApplicationController < ActionController::Base
   include Pundit
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
- 
+
+
+  # helpers methos
   helper_method :mobile_device?  
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  #filters
   skip_before_filter  :verify_authenticity_token
   before_action :configure_permitted_parameters, if: :devise_controller?  
 
