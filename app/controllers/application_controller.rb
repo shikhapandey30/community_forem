b88306c942d908d0a1ea8846202c14c657313890
hemant <hemant_gupta@seologistics.com>
2016-01-12 11:11:28 +0530

    fix bugs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-class ApplicationController < ActionController::Base
+# class ApplicationController < ActionController::Base
   include Pundit
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
